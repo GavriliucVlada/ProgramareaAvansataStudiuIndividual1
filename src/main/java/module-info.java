0fe1e0e197f93c931d01d6a0bc78f720dc46359d
GavriliucVlada <gavriliucvladap2212@gmail.com>
2025-10-03 09:38:51 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module ceiti.studiuindividual1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens ceiti.studiuindividual1 to javafx.fxml;
+    exports ceiti.studiuindividual1;
+}
